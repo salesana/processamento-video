6d0f1fe8d4e2c9481fd2bb207fa4444e586969ec
salesana <145409717+salesana@users.noreply.github.com>
2024-10-17 20:33:51 -0300

    Add files via upload

---
+++ laboratorio02/codana/codigo_7x7_save_image/CMakeFiles/Smothing.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ufabc/Documentos/anasales/lab02/codigo_7x7_save_image/Smothing.cpp" "CMakeFiles/Smothing.dir/Smothing.cpp.o" "gcc" "CMakeFiles/Smothing.dir/Smothing.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
