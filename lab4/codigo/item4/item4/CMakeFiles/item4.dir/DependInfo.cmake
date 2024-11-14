41df81cbe06409a9519c17e3076c2a62cbeba448
salesana <145409717+salesana@users.noreply.github.com>
2024-11-14 20:03:10 -0300

    Add files via upload

---
+++ lab4/codigo/item4/item4/CMakeFiles/item4.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ufabc/Documentos/chnunes/lab4/item4/item4.cpp" "CMakeFiles/item4.dir/item4.cpp.o" "gcc" "CMakeFiles/item4.dir/item4.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab4/codigo/item4/item4/CMakeFiles/item4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/item4.dir/item4.cpp.o"
+  "CMakeFiles/item4.dir/item4.cpp.o.d"
+  "item4"
+  "item4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/item4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
